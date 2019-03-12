4bae55f4f6ca4166399644d85c0d7f1014f4cddc
zhangzhichao <zhangzhichaolove@vip.qq.com>
2019-03-12 09:31:46 +0800

    create

--- app/src/main/java/pager/peakchao/com/peakpager/MainActivity.java
+++ app/src/main/java/pager/peakchao/com/peakpager/MainActivity.java
@@ -1,10 +1,10 @@
 package pager.peakchao.com.peakpager;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
